48179b085b852d7f2e96df3414ca35508dacc30e
cyoonique <choi.yoon333@gmail.com>
2020-04-27 16:17:07 -0700

    first commit

---
+++ myWebsite/server/server.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Importing routers
+
+// DB config here
+// const db = require()
+const PORT = 3000;
+const app = express();
+
+// Connecting to DB
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// serve static files
+app.use('/assets', express.static(path.join(__dirname, '../client/assets')));
+
+// app.use(express.static('assets'));
+
+/**
+ * define route handlers
+ */
+//if production
+if (process.env.NODE_ENV === 'production') {
+    // statically serve everything in the build folder on the route '/build'
+    app.use('/build', express.static(path.resolve(__dirname, '../build')));
+    // serve index.html on the route '/'
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../client/index.html'));
+    });
+};
+
+// serve html page
+app.get('/',  (req, res)=>{
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.sendStatus(404));
+
+// Global error handler
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`)
+});
+
+module.exports = app;
